9e42d5e8e7d5ca576e146787b6b7eb869c181af9
Jachimike Divine Ezenwamadu <94161891+JachiEze@users.noreply.github.com>
2024-03-21 16:30:40 -0400

    Update fullsmartcane.py

--- fullsmartcane.py
+++ fullsmartcane.py
@@ -7,7 +7,6 @@ import urllib.error
 import time
 from GPS_API import *  # Assuming GPS_API module is available in your environment
 import serial
-from gpiozero import InputDevice, OutputDevice, PWMOutputDevice
 import Adafruit_ADS1x15
 import pygame
 
@@ -49,52 +48,6 @@ sleep = 2  # how many seconds to sleep between posts to the channel
 
 key = "VQ4O3RCYND2NE9UH"  # Thingspeak Write API Key
 msgdata = Message()  # Creates a Message Instance
-
-# GPIO setup for ultrasonic sensor
-trig = OutputDevice(4)
-echo = InputDevice(17)
-motor = PWMOutputDevice(14)
-
-ADC = Adafruit_ADS1x15.ADS1115()
-ADC_CHANNEL = 3
-GAIN = 1
-MIN_ADC_VALUE = 0
-MAX_ADC_VALUE = 32767
-
-# Initialize pygame mixer
-pygame.mixer.init()
-
-# Load the MP3 file
-water_file = "waterdetected.mp3"
-pygame.mixer.music.load(water_file)
-
-
-def get_pulse_time():
-    trig.on()
-    time.sleep(0.00001)
-    trig.off()
-
-    while echo.is_active == False:
-        pulse_start = time.time()
-
-    while echo.is_active == True:
-        pulse_end = time.time()
-
-    time.sleep(0.06)
-
-    return pulse_end - pulse_start
-
-
-def calculate_distance(duration):
-    speed = 343
-    distance = speed * duration / 2
-    return distance
-
-
-def calculate_vibration(distance):
-    vibration = (((distance - 0.02) * -1) / (4 - 0.02)) + 1
-    return vibration
-
 
 def getObjects(img, thres, nms, objects=[]):
     classIds, confs, bbox = net.detect(img, confThreshold=thres, nmsThreshold=nms)
@@ -151,19 +104,6 @@ try:
     last_water_check_time = time.time()
 
     while True:
-        # Water level detection every 2 seconds
-        if time.time() - last_water_check_time >= 2:
-            adc_value = ADC.read_adc(ADC_CHANNEL, gain=GAIN)
-            water_level = (adc_value - MIN_ADC_VALUE) / (MAX_ADC_VALUE - MIN_ADC_VALUE) * 100
-
-            print("ADC Value: {} | Water Level: {:.2f}%".format(adc_value, water_level))
-
-            # If water level is 25% or greater, play the MP3 file
-            if water_level >= 25:
-                pygame.mixer.music.play()
-
-            last_water_check_time = time.time()
-
         success, img = cap.read()
         result, objectInfo = getObjects(img, 0.60, 0.2)
         cv2.imshow("Output", img)
