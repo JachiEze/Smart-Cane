32b53c47be2b41117420ec9f9f050f0e09bb71f0
Jachimike Divine Ezenwamadu <94161891+JachiEze@users.noreply.github.com>
2024-04-01 02:20:31 -0400

    Update ultrasonicsensor.py

---
+++ ultrasonicsensor.py
@@ -0,0 +1,48 @@
+import RPi.GPIO as GPIO
+import time
+import pygame
+
+try:
+    GPIO.setmode(GPIO.BOARD)
+
+    PIN_TRIGGER = 7
+    PIN_ECHO = 11
+
+    GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+    GPIO.setup(PIN_ECHO, GPIO.IN)
+
+    GPIO.output(PIN_TRIGGER, GPIO.LOW)
+
+    print("Waiting for sensor to settle")
+    time.sleep(2)
+
+    # Initialize pygame mixer
+    pygame.mixer.init()
+
+    # Load the MP3 file
+    obstruction_file = "obstructiondetected.mp3"
+    pygame.mixer.music.load(obstruction_file)
+
+    while True:  # Run continuously
+        print("Calculating distance")
+
+        GPIO.output(PIN_TRIGGER, GPIO.HIGH)
+        time.sleep(0.00001)
+        GPIO.output(PIN_TRIGGER, GPIO.LOW)
+
+        while GPIO.input(PIN_ECHO) == 0:
+            pulse_start_time = time.time()
+        while GPIO.input(PIN_ECHO) == 1:
+            pulse_end_time = time.time()
+
+        pulse_duration = pulse_end_time - pulse_start_time
+        distance = round(pulse_duration * 17150, 2)
+        print("Distance:", distance, "cm")
+
+        if distance <= 100:
+            pygame.mixer.music.play()
+
+        time.sleep(2)  # Wait for 2 seconds before the next measurement
+
+finally:
+    GPIO.cleanup()
